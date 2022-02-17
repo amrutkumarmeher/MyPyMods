74dad89bdd2b99083e0b01be75a20c511f7a62ad
ROYAL_AEO <80609498+Royals-Aeo-Gamer@users.noreply.github.com>
2022-02-17 22:26:11 +0530

    link list

--- Modules/LList.py
+++ Modules/LList.py
@@ -1,50 +1,81 @@
 # Node class
 class node:
-    def __init__(self,data,next):
+    def __init__(self, data, next):
         self.data = data
         self.next = next
+
 
 # Link list class (main class)
 class llist:
     def __init__(self):
         self.top = None
-    def append(self,data):
+
+    def append(self, data):
         if self.top == None:
-            self.top = node(data,None)
+            self.top = node(data, None)
         else:
             itr = self.top
-            while itr!=None:
+            while itr != None:
                 global prev
                 prev = itr
                 itr = itr.next
-            prev.next = node(data,None)
-    def insert(self,data,index):
+            prev.next = node(data, None)
+
+    def insert(self, data, index):
         itr = self.top
         count = 0
         prev = None
-        while itr!= None:
+        while itr != None:
             if index == 0:
-                self.top = node(data,itr)
+                self.top = node(data, itr)
                 break
             elif count == index:
                 e = itr
-                prev.next = node(data,e)
+                prev.next = node(data, e)
                 break
-            count+=1
+            count += 1
             prev = itr
             itr = itr.next
 
     def show_list(self):
         itr = self.top
-        while itr!=None:
+        while itr != None:
             yield itr.data
             itr = itr.next
+
+    def remove(self, element):
+        itr = self.top
+        prev = None
+        while itr != None:
+            if element == self.top.data:
+                self.top = itr.next
+            elif itr.data == element:
+                prev.next = itr.next
+                break
+            prev = itr
+            itr = itr.next
+
+    def remove_by_index(self, index):
+        itr = self.top
+        count = 0
+        prev = None
+        while itr != None:
+            if index == 0:
+                self.head = itr.next
+            elif count == index:
+                prev.next = itr.next
+            prev = itr
+            count += 1
+            itr = itr.next
+    def
 
 if __name__ == "__main__":
     ll = llist()
     ll.append(2)
     ll.append(6)
     print(list(ll.show_list()))
-    ll.insert(4,1)
-    ll.insert("nothing",0)
+    ll.insert(4, 1)
+    ll.insert("nothing", 0)
+    print(list(ll.show_list()))
+    ll.remove("nothing")
     print(list(ll.show_list()))
