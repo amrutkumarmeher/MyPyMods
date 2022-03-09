e2e9853e7fa5362ec5cce83c094d8d8cb3a73982
ROYAL_AEO <80609498+Royals-Aeo-Gamer@users.noreply.github.com>
2022-03-09 23:01:00 +0530

    completed!

--- Modules/Tree.py
+++ Modules/Tree.py
@@ -1,24 +1,34 @@
 class TreeNode:
-	def __init__(self,name,data,parent=None):
-		self.name = name
-		self.parent = parent
-		self.data = data
-		self.childs = []
-	def add_child(self,name,data):
-		self.childs.append((type(self))(name,data,self))
-	
-	def rm_branch(self,ansistors_N,name,level):
-		focus = self.childs
-		while True:
-			if ansistors_N[0] in focus:
-				focus = focus[ansistors_N[0]]
-		        del ansistors_N[0]
-		    elif name in focus and ansistors == None:
-		        del focus[name]
-		        break
+    def __init__(self, name, data, parent=None):
+        self.name = name
+        self.parent = parent
+        self.data = data
+        self.childs = {}
 
-if __name__ == "__main__":
-	ins = TreeNode("first","hi")
-	ins.add_child("second","hello")
-	ins.add_child("second"	,"hee")
-	
+    def add_child(self, name, data):
+        self.childs.update({name:(type(self))(name, data, self)})
+
+    def rm_branch(self, name, ansistors_n: list = None,):
+        focus = self.childs
+        while True:
+            if ansistors_n == None or ansistors_n == self.name:
+                del focus[name]
+                break
+            elif ansistors_n[0] in focus:
+                focus = (focus[ansistors_n[0]]).childs
+                del ansistors_n[0]
+            elif name in focus and ansistors_n is None:
+                del focus[name]
+                break
+            else:
+                print(focus)
+                raise NameError(f"couldn't find branch {ansistors_n[0]}")
+    def __getitem__(self, item):
+        return self.childs[item]
+
+    def __setitem__(self, key, value):
+        self.childs[key] = value
+
+    def __delitem__(self, key, ansistors_n: list = None):
+        self.rm_branch(key, ansistors_n)
+
