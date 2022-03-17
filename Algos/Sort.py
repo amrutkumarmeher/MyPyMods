c95b85e28bea0ea9da9852b7524f9548a27bdf77
ROYAL_AEO <80609498+Royals-Aeo-Gamer@users.noreply.github.com>
2022-03-17 15:35:31 +0530

    Bubble sort

--- Algos/Sort.py
+++ Algos/Sort.py
@@ -1,7 +1,23 @@
-def SelectionSort(array):
+def SelectionSort(array,small_to_big=True):
     for i in range(len(array)):
-        smallest = i
+        hold = i
         for ie in range(i+1,len(array)):
-            if array[ie] < array[smallest]:
-                smallest = ie
-        array[i],array[smallest] = array[smallest],array[i]
+            if array[ie] < array[hold] and small_to_big==True:
+                hold = ie
+            elif array[ie] > array[hold] and small_to_big==False:
+                hold = ie
+        array[i],array[hold] = array[hold],array[i]
+
+def bubbleSort(array,small_to_big=True):
+    for i in range(len(array) - 1):
+        next = 0
+        for i in range(len(array)-1):
+            next += 1
+            if array[next] < array[i] and small_to_big == True:
+                var = array[next]
+                array[next] = array[i]
+                array[i] = var
+            elif array[next] > array[i] and small_to_big == False:
+                var = array[next]
+                array[next] = array[i]
+                array[i] = var
